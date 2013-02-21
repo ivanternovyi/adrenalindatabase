1ec252480347ed91ad2e305dd97ed846921dfcb2
Alexey <aadonskoy@gmail.com>
2013-02-21 13:26:08 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Adrenalindatabase::Application.config.secret_token = '5cc0fed14d7795673f5295706ca4420c13724e56af7b37cae7a9ff26a058698e5f86a0159b777d396ba24f7c83699e770fa3ecf5af8f4978612e08ff0fa58947'
