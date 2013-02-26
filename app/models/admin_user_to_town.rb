381f5145bb9a0b85f9e94b297f72fb9d837a2bd0
Alexey <aadonskoy@gmail.com>
2013-02-26 16:44:33 +0200

    Set dependencies to user, town offices models

--- app/models/admin_user_to_town.rb
+++ app/models/admin_user_to_town.rb
@@ -1,3 +1,6 @@
 class AdminUserToTown < ActiveRecord::Base
   attr_accessible :town_id, :user_id
+
+  belongs_to :user
+  belongs_to :town
 end
--- app/models/town.rb
+++ app/models/town.rb
@@ -1,5 +1,15 @@
 class Town < ActiveRecord::Base
-  attr_accessible :name, :office
+  after_update :remove_unchecked_offices
+  attr_accessible :name, :office, :users
+
+  has_many :admin_user_to_towns, dependent: :destroy
+  has_many :users, through: :admin_user_to_towns
 
   scope :with_office, where(office: true)
+
+  private
+  def remove_unchecked_offices
+    towns = Town.where(office: false).collect {|t| t.id}
+    UserToAdminRegion.where(town_id: towns).destroy_all
+  end
 end
