6088e78b3ffe01290a31d5ca33e5bd56cf6e6bd5
Alexey Donskoy <aadonskoy@gmail.com>
2013-02-28 16:43:33 +0200

    Test User with rspec beginning

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) {User.new}
+
+  it "should require email" do
+  	user.email = nil
+  	user.should_not be_valid
+  	user.errors.on(:email).should_not be_valid
+  end
 end
