b29930f97213bb628794a48aa71b82251826e598
Alexey Donskoy <aadonskoy@gmail.com>
2013-03-06 10:12:39 +0200

    Add Ukrainian localization

--- config/application.rb
+++ config/application.rb
@@ -37,11 +37,13 @@ module Adrenalindatabase
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :en
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
+    # Setting Ukrainian locale
+    config.i18n.locale = :ua
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
