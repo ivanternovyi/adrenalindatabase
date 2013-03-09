1251f432c4125018f32f208b6ccd25fe2974a4e2
Alexey <aadonskoy@gmail.com>
2013-03-09 14:28:16 +0200

    Add tests for model UserDetail and Role

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe Role do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should not be valid without name" do
+    FactoryGirl.build(:role, name: '').should_not be_valid
+  end
+
+  it "should add valid record to database" do
+    count_records = Role.all.count
+    FactoryGirl.create(:role, name: '')
+    Role.all.count.should be(count_records + 1)
+  end
 end
--- spec/models/user_details_spec.rb
+++ spec/models/user_details_spec.rb
@@ -6,4 +6,28 @@ describe UserDetail do
     FactoryGirl.create(:user_detail)
     UserDetail.all.count.should be(count_usr_detail + 1)
   end
+
+  it "should not be valid without name" do
+    FactoryGirl.build(:user_detail, name: nil).should_not be_valid
+  end
+
+  it "should not be valid without surname" do
+    FactoryGirl.build(:user_detail, surname: nil).should_not be_valid
+  end
+
+  it "should not be valid without mid_name" do
+    FactoryGirl.build(:user_detail, mid_name: nil).should_not be_valid
+  end
+
+  it "should not be valid without birthday" do
+    FactoryGirl.build(:user_detail, birthday: nil).should_not be_valid
+  end
+
+  it "should not be valid without post_address" do
+    FactoryGirl.build(:user_detail, post_address: nil).should_not be_valid
+  end
+
+  it "shoud not be valid with wrong birthday" do
+    FactoryGirl.build(:user_detail, birthday: 'data').should_not be_valid
+  end
 end
