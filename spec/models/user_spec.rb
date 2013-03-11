0b6657c72a75da4ff5f51c450d8ec0f8a29f22fb
Alexey Donskoy <aadonskoy@gmail.com>
2013-03-11 12:53:23 +0200

    Write test for destroying belonging records card_infos

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,13 +24,13 @@ describe User do
 
   describe "associated models" do
     describe "user_detail" do
-      it "should have one user detail" do
+      it "should have one record" do
         usr_det = FactoryGirl.create(:user_detail)
         usr = FactoryGirl.create(:user, user_detail: usr_det)
         usr.user_detail.should eql(usr_det)
       end
 
-      it "shoud destroy belinging user_detail" do
+      it "shoud destroy belonging user_detail" do
         usr_det = FactoryGirl.create(:user_detail)
         usr_det_id = usr_det.id
         usr = FactoryGirl.create(:user, user_detail: usr_det)
@@ -40,21 +40,29 @@ describe User do
     end
 
     describe "card_infos" do
-      it "should have many card_infos" do
+      it "should have many records" do
         usr = FactoryGirl.create(:user)
         3.times{ usr.card_infos << FactoryGirl.create(:card_info) }
         usr.card_infos.count.should be(3)
       end
+
+      it "should destroy when user destroy" do
+        usr = FactoryGirl.create(:user)
+        3.times{ usr.card_infos << FactoryGirl.create(:card_info) }
+        card_ids = usr.card_infos.collect { |ph_id| ph_id.id }
+        usr.destroy
+        card_ids.each { |id| CardInfo.find_by_id(id).should be_nil }
+      end
     end
 
     describe "phones" do
-      it "should have many phones" do
+      it "should have many records" do
         usr = FactoryGirl.create(:user)
         3.times{ usr.phones << FactoryGirl.create(:phone) }
         usr.phones.count.should be(3)
       end    
 
-      it "should delete belonging phones" do
+      it "should destroy when user destroy" do
         usr = FactoryGirl.create(:user)
         3.times{ usr.phones << FactoryGirl.create(:phone) }
         ph_ids = usr.phones.collect { |ph_id| ph_id.id }
