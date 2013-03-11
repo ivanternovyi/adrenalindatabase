601007ce3900e0857ff11cad4a73e19e15a8a56b
Alexey Donskoy <aadonskoy@gmail.com>
2013-03-11 11:19:51 +0200

    Add tests for phone model

--- spec/models/phone_spec.rb
+++ spec/models/phone_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe Phone do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should be valid data" do
+    FactoryGirl.build(:phone).should be_valid
+  end
+
+  it "should not empty field phone_number" do
+    FactoryGirl.build(:phone, phone_number: nil).should_not be_valid
+  end
+
+  it "should be not valid phone_number" do
+    FactoryGirl.build(:phone, phone_number: '10').should_not be_valid
+    FactoryGirl.build(:phone, phone_number: '2357a83021').should_not be_valid
+    FactoryGirl.build(:phone, phone_number: '274926592').should_not be_valid
+    FactoryGirl.build(:phone, phone_number: '+3832165204322').should_not be_valid
+    FactoryGirl.build(:phone, phone_number: '+38321652a322').should_not be_valid
+  end
 end
