92c226ae66c4033ea95e56208d217875e26f4f82
Alexey Donskoy <aadonskoy@gmail.com>
2013-03-11 13:20:56 +0200

    Write test for contacts

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -4,8 +4,4 @@ describe Contact do
   it "should be valid" do
     FactoryGirl.build(:contact).should be_valid
   end
-
-  it "should be invalid" do
-    FactoryGirl.build(:contact, icq_number: 'sdssd').should_not be_valid
-  end
 end
