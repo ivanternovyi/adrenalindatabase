4d0b553e05c710e8094e53abffb81448b1010ac6
Alexey <aadonskoy@gmail.com>
2013-03-15 16:21:37 +0200

    Test for requests: user can view his profile

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,11 +1,26 @@
+# encoding: utf-8
 require 'spec_helper'
 
 describe "Users" do 
   include Capybara::DSL
   include Capybara::RSpecMatchers
   
-  it "cause log in user" do
+  it "should cause log in user" do
     visit '/'
-    page.should have_content "Not authorized!"    
+    page.should have_content "You are not authorized to access this page."    
+  end
+
+  it "should create new user" do
+    visit '/users/new'
+    page.should have_content "Новый учасник"
+  end
+
+  it "should redirect to user profile if regular user exist and login" do
+    user = FactoryGirl.create(:user, role_id: Role.find_by_name('RegularUser').id)
+    visit '/'
+    fill_in "Номер карти або e-mail", with: user.email
+    fill_in "Пароль", with: user.password
+    click_button 'Вхід'
+    page.should have_content "SHOW IT"
   end
 end
