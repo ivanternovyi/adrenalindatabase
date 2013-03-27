286d4d6d1b4efd8c6b4b25f6aabbd2e74301fd19
Alexey <aadonskoy@gmail.com>
2013-03-28 00:22:59 +0200

    Teach writing Cucumber tests

--- features/step_definitions/regular_user_steps.rb
+++ features/step_definitions/regular_user_steps.rb
@@ -2,4 +2,14 @@ Given /^I am on (.+)$/ do |page_name|
   visit path_to(page_name)
 end
 
-# When /^I fill in (.+)$/ do ||
+When /^I go to (.+)$/ do |page_name|
+  visit path_to(page_name)
+end
+
+When /^I press "([^\"]*)"$/ do |button|
+  click_button(button)
+end
+
+When /^I click "([^\"]*)"$/ do |link|
+  click_link(link)
+end
