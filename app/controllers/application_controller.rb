948a29991d82919cc3e3a978f362cf0d569ff5ca
Alexey Donskoy <aadonskoy@gmail.com>
2013-03-28 13:20:16 +0200

    Translate access denied message to ukrainian.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
+# encoding: utf-8
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
   rescue_from CanCan::AccessDenied do |exception|
-    redirect_to new_user_session_path, :alert => exception.message
+    redirect_to new_user_session_path, :alert => 'Ви не можете бачити цю сторінку. Увійдіть будь-ласка.'
   end
 
   def after_sign_in_path_for(resource)
