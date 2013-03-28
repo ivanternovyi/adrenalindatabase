8f24b01b6413617abcf51e76d884e5d1ba1e7ae7
Alexey Donskoy <aadonskoy@gmail.com>
2013-03-28 11:44:40 +0200

    Set timestamp_registration on creating user event

--- app/models/user_detail.rb
+++ app/models/user_detail.rb
@@ -4,9 +4,15 @@ class UserDetail < ActiveRecord::Base
 
   belongs_to :user
 
+  before_create :set_registration_timestamp
+
   validates_presence_of :surname, message: 'Прізвище не повинно бути порожнім'
   validates_presence_of :name, message: 'Ім\'я не повинно бути порожнім'
   validates_presence_of :mid_name, message: 'По-Батькові не повинно бути порожнім'
   validates_presence_of :post_address, message: 'Поштова адреса не може бути порожньою'
   validates_presence_of :birthday, message: 'Дата народження не може бути порожньою'
+
+  def set_registration_timestamp
+    self.registration_timestamp = DateTime.now
+  end
 end
