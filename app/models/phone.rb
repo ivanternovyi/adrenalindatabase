7b6472590668006fe65f07e3852d95e3f475c67f
Alexey Donskoy <aadonskoy@gmail.com>
2013-04-01 18:59:03 +0300

    Localization phone validations messages.

--- app/models/phone.rb
+++ app/models/phone.rb
@@ -1,10 +1,11 @@
+# encoding: utf-8
 class Phone < ActiveRecord::Base
   attr_accessible :main, :phone_number, :user_id
 
   before_save :phone_clean
 
-  validates_presence_of :phone_number
-  validates_format_of :phone_number, with: /^\+\d{1,3}\s\([0-9]{3}\)\s[0-9]{3}\-[0-9]{2}\-[0-9]{2}$/
+  validates_presence_of :phone_number, message: 'Не може бути порожнім'
+  validates_format_of :phone_number, with: /^\+\d{1,3}\s\([0-9]{3}\)\s[0-9]{3}\-[0-9]{2}\-[0-9]{2}$/, message: 'Неправильний номер'
 
   belongs_to :user
 
