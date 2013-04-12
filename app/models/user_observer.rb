769991cb5232eaf89f47f03600e1ae67a8a22427
Alexey Donskoy <aadonskoy@gmail.com>
2013-04-12 23:55:29 +0300

    Chack on existing user before mailing

--- app/models/user_observer.rb
+++ app/models/user_observer.rb
@@ -1,6 +1,11 @@
 class UserObserver < ActiveRecord::Observer
-  def after_create(model)
-    if model.new_record?
+  def before_save(model)
+    @was_a_new_record = model.new_record?
+    return true
+  end
+
+  def after_save(model)
+    if @was_a_new_record
       if File.exist?("#{Rails.root}/tmp/pids/delayed_job.pid")
         NotifyMailer.delay.notify_register(model)
       else
