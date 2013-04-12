e51716030e52d1989f85f9d4ab08c6344e43068d
Alexey Donskoy <aadonskoy@gmail.com>
2013-04-13 00:01:50 +0300

    Remove unnecessary variables.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -24,8 +24,6 @@ Adrenalindatabase::Application.configure do
   # Disable request forgery protection in test environment
   config.action_controller.allow_forgery_protection    = false
 
-  config.mail_delay = false
-
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
