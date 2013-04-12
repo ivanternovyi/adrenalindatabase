19a857f38563e48e02439d3337a21d3d93f5fd66
Alexey Donskoy <aadonskoy@gmail.com>
2013-04-12 23:48:12 +0300

    Set environment for production and development mail

--- app/models/user_observer.rb
+++ app/models/user_observer.rb
@@ -1,5 +1,11 @@
 class UserObserver < ActiveRecord::Observer
-  def before_save(model)
+  def after_create(model)
+    if model.new_record?
+      if File.exist?("#{Rails.root}/tmp/pids/delayed_job.pid")
         NotifyMailer.delay.notify_register(model)
+      else
+        NotifyMailer.notify_register(model).deliver
+      end
+    end
   end
 end
