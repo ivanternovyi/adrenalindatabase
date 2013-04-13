7619f45651248cf258c52d6af7cf315429b3c38c
Alexey Donskoy <aadonskoy@gmail.com>
2013-04-13 20:30:43 +0300

    Commit with failed tests

--- features/support/env.rb
+++ features/support/env.rb
@@ -9,6 +9,7 @@ require 'database_cleaner'
 require 'database_cleaner/cucumber'
 require Rails.root + 'spec/factories'
 require 'capybara'
+require 'email_spec/cucumber'
 # Capybara defaults to CSS3 selectors rather than XPath.
 # If you'd prefer to use XPath, just uncomment this line and adjust any
 # selectors in your step definitions to use the XPath syntax.
