708708e1a7374930bf78ce24ab41978b53314ca4
Alexey Donskoy <alexey@Temple.local>
2013-10-02 13:54:47 +0300

    Fixed error with empty array.

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,8 +3,14 @@ class Trip < ActiveRecord::Base
   has_many :trip_dates, dependent: :destroy
 
   def get_unregistered_dates(user)
-  	return TripDate.where('trip_id = ? AND id NOT IN (?)', self.id, 
-  		UserTripOrder.where('user_id = ?', user.id).collect{ |usr| usr.trip_date_id }) unless user.nil?
-  	return self.trip_dates if user.nil?
+    unless user.nil?
+      registered_tr_dat_ids = UserTripOrder.where('user_id = ?', user.id).collect{ |usr| usr.trip_date_id }
+      registered_tr_dat_ids = '' unless registered_tr_dat_ids.any?
+      trip = TripDate.where('trip_id = ? AND id NOT IN (?)', self.id, registered_tr_dat_ids)
+    end
+
+    trip = self.trip_dates if user.nil?
+
+    trip
   end
 end
