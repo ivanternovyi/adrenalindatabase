bdadc4a7a0db1df406ff7d200358aee55f5068db
Alexey Donskoy <aadonskoy@gmail.com>
2013-10-11 16:19:48 +0300

    Completed to create payments for trip managements.

--- app/controllers/admin_trip_order_payments_controller.rb
+++ app/controllers/admin_trip_order_payments_controller.rb
@@ -4,10 +4,35 @@ class AdminTripOrderPaymentsController < ApplicationController
 
   def index
     @payments = get_order.user_payment_trip_orders
+    @payment = get_order.user_payment_trip_orders.build
+  end
+
+  def create
+    @payment = get_order.user_payment_trip_orders.build(params[:user_payment_trip_order])
+    if @payment.save
+      @payments = get_order.user_payment_trip_orders
+      @payment = get_order.user_payment_trip_orders.build
+    end
+  end
+
+  def edit
+    @payments = get_order.user_payment_trip_orders
+    @payment = get_order.user_payment_trip_orders.find params[:id]
+  end
+
+  def update
+    @payment = get_order.user_payment_trip_orders.find params[:id]
+    if @payment.update_attributes(params[:user_payment_trip_order])
+      @payments = get_order.user_payment_trip_orders
+      @payment = get_order.user_payment_trip_orders.build
+    end
   end
 
   def destroy
-    @payment = get_order.find params[:id]
+    @payment = get_order.user_payment_trip_orders.find params[:id]
+    @payment.destroy
+    @payments = get_order.user_payment_trip_orders
+    @payment = get_order.user_payment_trip_orders.build
   end
 
   private
