cdd04566ff71b42c10e04fa55b1e49661bc08334
Alexey Donskoy <aadonskoy@gmail.com>
2013-10-29 15:09:20 +0200

    Add gem roo for importing xlsx files.

--- db/schema.rb
+++ db/schema.rb
@@ -58,6 +58,22 @@ ActiveRecord::Schema.define(:version => 20131007091614) do
   add_index "contacts", ["skype_name"], :name => "index_contacts_on_skype_name"
   add_index "contacts", ["user_id"], :name => "index_contacts_on_user_id"
 
+  create_table "delayed_jobs", :force => true do |t|
+    t.integer  "priority",   :default => 0
+    t.integer  "attempts",   :default => 0
+    t.text     "handler"
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
+
   create_table "payment_infos", :force => true do |t|
     t.integer  "card_info_id"
     t.date     "payment_date"
